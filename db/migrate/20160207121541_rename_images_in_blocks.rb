639006691cbb5bbfb827210b8298cd89776fef29
Laurence Berry <berry.laur@gmail.com>
2016-02-09 13:54:22 +0900

    Changes migration for block images

--- db/migrate/20160207121541_rename_images_in_blocks.rb
+++ db/migrate/20160207121541_rename_images_in_blocks.rb
@@ -1,9 +1,12 @@
 class RenameImagesInBlocks < ActiveRecord::Migration
-  def change
+  def up
     add_attachment :blocks, :image_primary
     add_attachment :blocks, :image_secondary
     add_attachment :blocks, :image_tertiary
 
+  end
+
+  def down
     remove_attachment :blocks, :image_primary
     remove_attachment :blocks, :image_secondary
     remove_attachment :blocks, :image_tertiary
