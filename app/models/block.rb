baec2d8b627edaa9a55f36762cbcfc76302234a5
Laurence Berry <berry.laur@gmail.com>
2016-02-10 12:45:47 +0900

    Improve styling for projects show

--- app/models/block.rb
+++ app/models/block.rb
@@ -1,10 +1,10 @@
 class Block < ActiveRecord::Base
   belongs_to :project
-  has_attached_file :image, styles: { large: "1280x1024", medium: "300x300>" }
+  has_attached_file :image, styles: { large: "960x640", medium: "476x320>" }
 
-  has_attached_file :image_primary, styles: { large: "1280x1024", medium: "300x300>" }
-  has_attached_file :image_secondary, styles: { large: "1280x1024", medium: "300x300>" }
-  has_attached_file :image_tertiary, styles: { large: "1280x1024", medium: "300x300>" }
+  has_attached_file :image_primary, styles: { large: "960x640", medium: "476x320>" }
+  has_attached_file :image_secondary, styles: { large: "960x640", medium: "476x320>" }
+  has_attached_file :image_tertiary, styles: { large: "960x640", medium: "476x320>" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
   validates_attachment_content_type :image_primary, content_type: /\Aimage\/.*\Z/
   validates_attachment_content_type :image_secondary, content_type: /\Aimage\/.*\Z/
