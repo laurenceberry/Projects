5b48098fe862a88591ec527d9aebbb94e01085ab
Laurence Berry <berry.laur@gmail.com>
2016-02-13 13:25:56 +0900

    Increases image size for large images

--- app/models/block.rb
+++ app/models/block.rb
@@ -2,7 +2,7 @@ class Block < ActiveRecord::Base
   belongs_to :project
   has_attached_file :image, styles: { large: "960x640", medium: "476x320>" }
 
-  has_attached_file :image_primary, styles: { large: "960x640", medium: "476x320>" }
+  has_attached_file :image_primary, styles: { large: "1440x960", medium: "476x320>" }
   has_attached_file :image_secondary, styles: { large: "960x640", medium: "476x320>" }
   has_attached_file :image_tertiary, styles: { large: "960x640", medium: "476x320>" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
