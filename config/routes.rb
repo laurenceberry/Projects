7d276e1a1883c93052cf331450b4e58d815d861a
laurenceberry <berry.laur@gmail.com>
2017-05-01 17:58:55 +0100

    Remove projects path

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,11 @@ Rails.application.routes.draw do
   get '/about'     => 'pages#about'
   get '/scrapbook' => 'projects#scrapbook'
   get '/weeknotes' => 'projects#weeknotes'
-  resources :projects
 
   root 'projects#index'
 
   get '/change_locale/:locale', to: 'settings#change_locale', as: :change_locale
+
+  resources :projects, path: ""
 
 end
